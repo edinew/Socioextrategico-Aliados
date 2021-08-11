fadab8b3df09476612fe1948766e32c40bb25fd3
edinew <edisonalvarez.ca1996@gmail.com>
2021-08-10 21:51:50 -0500

    CSS, IMG actaualizadas

--- login.php
+++ login.php
@@ -5,14 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="/login.css">
+    <link rel="stylesheet" href="CSS/login.css">
 </head>
 <body>
     
     
     <div class="login">
         <div class="logoUsuario">
-            <img src="/img/usuario.png" alt="logo">
+            <img src="IMG/usuario.png" alt="logo">
         </div>
         <form action="">
             <label for="usuario">Usuario</label>
